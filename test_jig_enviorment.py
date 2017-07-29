e0f69a16d5ea0baccf0a12570aebeafc64c1e1ff
davidjun <davejun@gmail.com>
2017-07-29 04:44:52 -0500

    added mpu6050 support to main data collection script

--- test_jig_enviorment.py
+++ test_jig_enviorment.py
@@ -8,13 +8,16 @@ import RPi.GPIO as GPIO
 from flask import Flask
 from multiprocessing import Process, Value
 import pickle
+from mpu6050 import mpu6050
 
 # Variables
 store_file = "results.csv"
-store_path = "/media/pi/983F-EB83"
+#store_path = "/media/pi/983F-EB83"
+store_path = "/home/pi/Github/brandonium21/results"
 storage_location = "{}/{}".format(store_path, store_file)
 current_temp = 0.0
 current_hum = 0.0
+sensor = mpu6050(0x68)
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
@@ -75,6 +78,12 @@ def GetData(start):
                     UpdateLed(float(temperature))
                     print ('Temp: {0:0.1f} C  Humidity: {1:0.1f} %'.format(temperature, humidity))
 
+# Get IMU data
+def GetIMU(start):
+    while True:
+        if start.value == True:
+            acc_data = sensor.get_accel_data()
+            print ('AccX: {0:0.2f} AccY: {1:0.2f} AccZ: {2:0.2f}'.format(acc_data['x'], acc_data['y'], acc_data['z']))
 
 # Serve dashboard website 
 app = Flask(__name__)
@@ -88,5 +97,8 @@ if __name__ == "__main__":
    start = Value('b', True)
    p = Process(target=GetData, args=(start,))
    p.start()  
+   q = Process(target=GetIMU, args=(start,))
+   q.start()  
    app.run(debug=True, use_reloader=False)
    p.join()
+   q.join()
